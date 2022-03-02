b681909edf1e334a4b07af2e317e5ce4e5dd4139
maxime.zammit <maxime.zammit@epfl.ch>
2022-03-02 21:01:58 +0100

    new CI attempt

--- app/src/androidTest/java/ch/sdp/vibester/EndToEndTest.kt
+++ app/src/androidTest/java/ch/sdp/vibester/EndToEndTest.kt
@@ -1,16 +1,10 @@
 package ch.sdp.vibester
 
-import android.app.Activity
-import android.app.Instrumentation
-import android.content.Intent
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.Intents.intending
-import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -28,18 +22,17 @@ import org.junit.runner.RunWith
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class EndToEndTest {
 
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("ch.sdp.vibester", appContext.packageName)
-
     }
 
     @get:Rule
-    var testRule = ActivityScenarioRule(
+    val testRule = ActivityScenarioRule(
         MainActivity::class.java
     )
 
@@ -47,33 +40,10 @@ class ExampleInstrumentedTest {
     @Test
     fun endToEndTest() {
         // Context of the app under test.
-        val name = "Maxime"
+        val name = "SDPUser"
         onView(withId(R.id.mainNameInput)).perform(ViewActions.typeText(name), closeSoftKeyboard())
         onView(withId(R.id.mainButton)).perform(click())
         onView(withId(R.id.greetName)).check(matches(withText("Hello $name!")))
     }
-    
-
-    /*
-    @Test
-    fun greetingActivity(){
-        // Build the result to return when the activity is launched.
-        //Intents.init()
-        val resultData = Intent()
-        val name = "MaximeZmt"
-        resultData.putExtra("name", name)
-        val result = Instrumentation.ActivityResult(Activity.RESULT_OK, resultData)
-
-        // Set up result stubbing when an intent sent to "contacts" is seen.
-        intending(toPackage("ch.sdp.vibester")).respondWith(result)
-        // User action that results in "contacts" activity being launched.
-        // Launching activity expects phoneNumber to be returned and displayed.
-        //onView(withId(R.id.pickButton)).perform(click())
-
-        // Assert that the data we set up above is shown.
-        onView(withId(R.id.greetName)).check(matches(withText(name)))
-        //Intents.release()
-    }
-     */
 
 }

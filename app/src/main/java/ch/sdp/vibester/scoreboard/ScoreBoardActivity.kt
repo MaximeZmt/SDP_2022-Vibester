c2dde17135c001b2432809bff3c795dc86fdf490
jiabaow <jiabaowenwendy@gmail.com>
2022-03-06 11:44:10 +0100

    implement hard-coded version of loadPlayers

--- app/src/main/java/ch/sdp/vibester/scoreboard/ScoreBoardActivity.kt
+++ app/src/main/java/ch/sdp/vibester/scoreboard/ScoreBoardActivity.kt
@@ -25,7 +25,15 @@ class ScoreBoardActivity : AppCompatActivity() {
     }
 
     private fun loadPlayers() {
-        //firebase
+        //TODO: replace by firebase
+        val players: List<Player> = listOf(
+            Player(111, "Brownie","", 687 ),
+            Player(222, "pancake", "", 498),
+            Player(333, "cookie", "", 593),
+            Player(444, "Scone", "", 659),
+            Player(555, "cheesecake", "", 678),
+        )
+        showPlayersPosition(players)
     }
 
     private fun showPlayersPosition(players: List<Player>) {

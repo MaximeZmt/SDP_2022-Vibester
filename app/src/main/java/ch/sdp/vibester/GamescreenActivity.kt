4909d3bc4e4dc02c42ea44ebf6afcc32b6a33fcf
zwierski <margaux.zwierski@epfl.ch>
2022-03-07 12:38:00 +0100

    created base files

---
+++ app/src/main/java/ch/sdp/vibester/GamescreenActivity.kt
@@ -0,0 +1,11 @@
+package ch.sdp.vibester
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GamescreenActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gamescreen)
+    }
+}

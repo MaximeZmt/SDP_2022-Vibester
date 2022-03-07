be2ba62c9da503db9a6e7cd9d65f0b45abea37ae
jiabaow <jiabaowenwendy@gmail.com>
2022-03-07 20:38:46 +0100

    redefined style and colors

--- app/src/main/java/ch/sdp/vibester/scoreboard/ScoreBoardActivity.kt
+++ app/src/main/java/ch/sdp/vibester/scoreboard/ScoreBoardActivity.kt
@@ -25,13 +25,13 @@ class ScoreBoardActivity : AppCompatActivity() {
     }
 
     private fun loadPlayers() {
-        //TODO: replace by firebase
+        //TODO: replace hard-coded players by firebase query
         val players: List<Player> = listOf(
             Player(111, "Brownie","https://images.app.goo.gl/QJFUc8JwEs5aNHVJA", 687 ),
-            Player(222, "pancake", "https://images.app.goo.gl/3BAnNtTn6isYGgQX9", 498),
-            Player(333, "cookie", "https://images.app.goo.gl/49955BeModw7Q1Ls5", 593),
-            Player(444, "Scone", "https://images.app.goo.gl/WiSfZupnUQrMZZfs9", 659),
             Player(555, "cheesecake", "https://images.app.goo.gl/STS3nKqSLdxogekF7", 678),
+            Player(444, "Scone", "https://images.app.goo.gl/WiSfZupnUQrMZZfs9", 659),
+            Player(333, "cookie", "https://images.app.goo.gl/49955BeModw7Q1Ls5", 593),
+            Player(222, "pancake", "https://images.app.goo.gl/3BAnNtTn6isYGgQX9", 498),
         )
         showPlayersPosition(players)
     }

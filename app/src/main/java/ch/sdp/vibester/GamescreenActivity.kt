9e1fcd4668a9a22dc9aace2af8fce3d7ababcc50
zwierski <margaux.zwierski@epfl.ch>
2022-03-09 10:39:56 +0100

    basic gamescreen and layout

--- app/src/main/java/ch/sdp/vibester/GamescreenActivity.kt
+++ app/src/main/java/ch/sdp/vibester/GamescreenActivity.kt
@@ -1,11 +1,42 @@
 package ch.sdp.vibester
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class GamescreenActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gamescreen)
+
+        /* number of players for the game
+            currently hardcoded as a placeholder
+            will be retrieved from intent launched from the game setup screen
+         */
+        val players = arrayOf("Kamila", "Jiabao", "Arda", "Laurynas", "Margaux", "Maxime")
+
+        val buzzers = findViewById<LinearLayout>(R.id.buzzersLayout)
+
+        val greetingIntent = Intent(this, GamescreenActivity::class.java)
+
+        var i = 1
+
+        val buttons = arrayOfNulls<Button>(players.size)
+
+        for (pName in players) {
+            val buttonName = "button" + i.toString()
+            val buttonId = resources.getIdentifier(buttonName, "id", packageName)
+            val button = findViewById<Button>(buttonId)
+            button.text = pName
+            buttons.set(i-1, button)
+            button.setOnClickListener {
+                startActivity(greetingIntent)
+            }
+            buzzers.addView(findViewById<Button>(buttonId))
+            i = i + 1
+        }
+
     }
 }

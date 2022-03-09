58771e426083e7cd58d0f8dff095fd1a55335cfa
Kamila Babayeva <kamifai14@gmail.com>
2022-03-09 23:02:34 +0100

    ProfileSetup.kt: initialize user data

--- app/src/main/java/ch/sdp/vibester/profile/ProfileSetup.kt
+++ app/src/main/java/ch/sdp/vibester/profile/ProfileSetup.kt
@@ -10,5 +10,18 @@ class ProfileSetup: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+    private fun setupProfile(user: UserProfile){
+        findViewById<TextView>(R.id.handle).text =  user.handle
+        findViewById<TextView>(R.id.username).text = user.username
+        findViewById<TextView>(R.id.totalGames).text = user.totalGames.toString()
+        findViewById<TextView>(R.id.correctSongs).text = user.correctSongs.toString()
+        findViewById<TextView>(R.id.bestScore).text = user.bestScore.toString()
+        findViewById<TextView>(R.id.ranking).text = user.ranking.toString()
+        Glide.with(this)
+            .load(image)
+            .circleCrop()
+            .into(binding.imageView)
     }
+
+
 }

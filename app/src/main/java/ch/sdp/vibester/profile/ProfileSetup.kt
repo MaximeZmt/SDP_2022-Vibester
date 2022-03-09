23f7a4be8f94342f429bafe59c94eb41184d5acc
Kamila Babayeva <kamifai14@gmail.com>
2022-03-09 23:01:44 +0100

    ProfileSetup.kt: set profile activity

---
+++ app/src/main/java/ch/sdp/vibester/profile/ProfileSetup.kt
@@ -0,0 +1,14 @@
+package ch.sdp.vibester.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import ch.sdp.vibester.R
+
+
+class ProfileSetup: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}

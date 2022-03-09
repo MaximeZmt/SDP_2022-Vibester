c5377cb259b0598cb793a1131e388ad69f6c820b
maxime.zammit <maxime.zammit@epfl.ch>
2022-03-09 18:52:09 +0100

    test fixes

--- app/src/androidTest/java/ch/sdp/vibester/api/ItunesMusicApiTest.kt
+++ app/src/androidTest/java/ch/sdp/vibester/api/ItunesMusicApiTest.kt
@@ -34,7 +34,7 @@ class ItunesMusicApiTest{
         var mediaPlayer = ItunesMusicApi.playAudio("https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/bc/71/fc/bc71fca4-e0bb-609b-5b6e-92296df7b4b6/mzaf_8907306752631175088.plus.aac.p.m4a")
         mediaPlayer.setOnPreparedListener(MediaPlayer.OnPreparedListener {
             mediaPlayer.start()
-            assertEquals(false, mediaPlayer.isPlaying)
+            assertEquals(true, mediaPlayer.isPlaying)
         })
 
     }

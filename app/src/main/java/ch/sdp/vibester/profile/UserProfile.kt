fc2f215606c3d7935e11f6d1635adbce074426a9
Kamila Babayeva <kamifai14@gmail.com>
2022-03-10 21:30:19 +0100

    UserProfile: put data as Serializable

--- app/src/main/java/ch/sdp/vibester/profile/UserProfile.kt
+++ app/src/main/java/ch/sdp/vibester/profile/UserProfile.kt
@@ -1,10 +1,12 @@
 package ch.sdp.vibester.profile
+//import kotlinx.serialization.Serializable
+import java.io.Serializable
 
-data class UserProfile(
+data class UserProfile (
     var handle: String = "",
     var username: String = "",
     var image: String = "",
     var totalGames: Int = 0,
     var bestScore: Int = 0,
     var correctSongs: Int = 0,
-    var ranking: Int = 0)
+    var ranking: Int = 0): Serializable{}

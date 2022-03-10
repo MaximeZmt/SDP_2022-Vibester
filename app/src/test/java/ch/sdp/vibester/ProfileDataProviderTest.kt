58e785053cfe07bf4128c3a2e1c8de8d43f894aa
Kamila Babayeva <kamifai14@gmail.com>
2022-03-10 17:16:57 +0100

    ProfileDataProviderTest.kt: check the function generate the profile data correctly

--- app/src/test/java/ch/sdp/vibester/ProfileDataProviderTest.kt
+++ app/src/test/java/ch/sdp/vibester/ProfileDataProviderTest.kt
@@ -2,7 +2,6 @@ package ch.sdp.vibester
 
 import ch.sdp.vibester.profile.ProfileDataProvider
 import ch.sdp.vibester.profile.UserProfile
-import org.hamcrest.MatcherAssert
 import org.junit.Assert
 import org.junit.Test
 
@@ -11,14 +10,25 @@ class ProfileDataProviderTest {
 
     @Test
     fun checkGetUserData(){
-        val userID = "0"
+        val userID = 0
         val users: List<UserProfile> = listOf(
             UserProfile("user0", "username0","https://bit.ly/3tO0k8W",  5, 8, 34),
             UserProfile("user1", "username1", "https://bit.ly/3sZhbXm", 5, 8, 34)
         )
-        val dataProvider = ProfileDataProvider(userID, users)
-        val finalUser = dataProvider.getUserData()
-        Assert.assertEquals(finalUser.bestScore, users[0].bestScore)
+        val scoreboard: List<Int> = listOf(1,2,3,4,5,6)
+        val dataProvider = ProfileDataProvider(userID.toString(), users, scoreboard)
+        val finalUser = dataProvider.getUserProfileData()
+
+        // To change on recursive equality of objects
+        Assert.assertEquals(finalUser.handle, users[userID].handle)
+        Assert.assertEquals(finalUser.username, users[userID].username)
+        Assert.assertEquals(finalUser.image, users[userID].image)
+        Assert.assertEquals(finalUser.bestScore, users[userID].bestScore)
+        Assert.assertEquals(finalUser.totalGames, users[userID].totalGames)
+        Assert.assertEquals(finalUser.correctSongs, users[userID].correctSongs)
+        Assert.assertEquals(finalUser.ranking, scoreboard[userID])
     }
+
+
 
 }

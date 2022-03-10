52b1bc7c47c661b855455c9dc156eb15e11ebf3d
zwierski <margaux.zwierski@epfl.ch>
2022-03-10 13:48:28 +0100

    cleanup code into functions + add popup

--- app/src/main/java/ch/sdp/vibester/GamescreenActivity.kt
+++ app/src/main/java/ch/sdp/vibester/GamescreenActivity.kt
@@ -21,59 +21,92 @@ class GamescreenActivity: AppCompatActivity() {
          */
         val players = arrayOf("Kamila", "Jiabao", "Arda", "Laurynas")
 
-        val buzzers = findViewById<LinearLayout>(R.id.buzzersLayout)
-        val scores = findViewById<LinearLayout>(R.id.scoresTable)
-
         val gamescreenIntent = Intent(this, GamescreenActivity::class.java)
 
+        // hardcoded test values
+        val song = "Demo"
+        val artist = "The Placeholders"
+
         val buildPopup = AlertDialog.Builder(this)
+        buildPopup.setTitle("Solution")
+        buildPopup.setMessage("The song was " + song + " by " + artist)
 
+        buildPopup.setPositiveButton("Correct") { dialog, which ->
+            Toast.makeText(applicationContext,
+                "Congrats!", Toast.LENGTH_SHORT).show()
+        }
 
-        var i = 1
+        buildPopup.setNegativeButton("Wrong") { dialog, which ->
+            Toast.makeText(applicationContext,
+                "Too bad!", Toast.LENGTH_SHORT).show()
+        }
 
-        val buttons = arrayOfNulls<Button>(players.size)
-
-        val viewsOfPoints = arrayOfNulls<TableRow>(players.size)
         val allPoints = arrayOf(1, 2, 3, 4)
+
+        buildScores(players, allPoints)
+        buildBuzzers(players, buildPopup)
+
+    }
+
+    /*
+    Programmatically builds the table of scores according to the number of players
+     */
+    private fun buildScores(players: Array<String>, allPoints: Array<Int>) {
+
+        val scores = findViewById<LinearLayout>(R.id.scoresTable)
+        val viewsOfPoints = arrayOfNulls<TableRow>(players.size)
+
+        var i = 0
 
         for (pName in players) {
 
-            //val scoreName = "score" + i.toString()
-            //val scoreId = resources.getIdentifier(scoreName, "id", packageName)
             val score = TableRow(this)
-            //score.id = scoreId
             score.gravity = Gravity.CENTER
+
             val nameView = TextView(this)
             nameView.text = pName
             nameView.height = 75
             nameView.width = 300
             nameView.gravity = Gravity.LEFT
+
             val points = TextView(this)
-            points.text = allPoints[i-1].toString()
+            points.text = allPoints[i].toString()
             points.height = 75
             points.width = 150
             points.gravity = Gravity.RIGHT
+
             score.addView(nameView)
             score.addView(points)
-            viewsOfPoints.set(i-1, score)
-            scores.addView(score)//,TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT))
-            //val buttonName = "button" + i.toString()
-            //val buttonId = resources.getIdentifier(buttonName, "id", packageName)
+            viewsOfPoints.set(i, score)
+            scores.addView(score)
+
+            i = i + 1
+        }
+    }
+
+    /*
+    Programmatically builds the buzzers according to the number and names of players.
+     */
+    private fun buildBuzzers(players: Array<String>, popup: AlertDialog.Builder) {
+
+        val buzzers = findViewById<LinearLayout>(R.id.buzzersLayout)
+        val buttons = arrayOfNulls<Button>(players.size)
+
+        var i = 0
+
+        for (pName in players) {
+
             val button = Button(this)
             button.text = pName
-            //button.id = buttonId
             button.width = 0
             button.height = 150
-            buttons.set(i-1, button)
+            buttons.set(i, button)
             button.setOnClickListener {
-
-                //startActivity(gamescreenIntent)
+                popup.show()
             }
             buzzers.addView(button)
 
             i = i + 1
         }
-
-
     }
 }

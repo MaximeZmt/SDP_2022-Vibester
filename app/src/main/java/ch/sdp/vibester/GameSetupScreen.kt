8e7fbca82511a4fcb33a42c7cbf35d20421a3d38
Derin Arda Alpay <d.arda.alpay@gmail.com>
2022-03-10 11:38:10 +0100

    Welcome page and game setup with no tests

---
+++ app/src/main/java/ch/sdp/vibester/GameSetupScreen.kt
@@ -0,0 +1,11 @@
+package ch.sdp.vibester
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameSetupScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_setup_screen)
+    }
+}
---
+++ app/src/main/java/ch/sdp/vibester/WelcomeScreen.kt
@@ -0,0 +1,11 @@
+package ch.sdp.vibester
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class welcome_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen)
+    }
+}

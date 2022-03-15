264ce1806d97d179e4c7e9d27e42994638bb8275
jiabaow <jiabaowenwendy@gmail.com>
2022-03-15 20:36:06 +0100

    add documentation

--- app/src/main/java/ch/sdp/vibester/model/Lyric.kt
+++ app/src/main/java/ch/sdp/vibester/model/Lyric.kt
@@ -6,6 +6,7 @@ import com.google.gson.annotations.SerializedName
 class Lyric {
     @SerializedName("lyrics")
     var lyrics: String? = null
+
     //not used
     //@SerializedName("error")
     //var error: String? = null

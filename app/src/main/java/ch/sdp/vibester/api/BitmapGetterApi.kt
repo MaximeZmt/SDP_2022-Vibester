bb1598aa79a21c7040282cbded6a3d980e0495f8
maxime.zammit <maxime.zammit@epfl.ch>
2022-03-15 08:55:35 +0100

    test for bitmapgetter

--- app/src/main/java/ch/sdp/vibester/api/BitmapGetterApi.kt
+++ app/src/main/java/ch/sdp/vibester/api/BitmapGetterApi.kt
@@ -1,14 +1,22 @@
 package ch.sdp.vibester.api
 
+import android.app.PendingIntent.getActivity
+import android.content.ContextWrapper
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import ch.sdp.vibester.R
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import java.io.IOException
-import java.lang.IllegalArgumentException
+import java.security.AccessController
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Executors
+
 
 class BitmapGetterApi private constructor(){
     companion object{
@@ -31,7 +39,6 @@ class BitmapGetterApi private constructor(){
             })
             return futureBitmap
         }
-
 
     }
 }

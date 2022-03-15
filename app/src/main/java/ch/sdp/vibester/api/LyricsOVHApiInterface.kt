f8505a26578cca63b1d32cc3e2b8a4ed414a649f
jiabaow <jiabaowenwendy@gmail.com>
2022-03-15 10:09:34 +0100

    refactor LyricsOVHApiInterface.kt and add tests

--- app/src/main/java/ch/sdp/vibester/api/LyricsOVHApiInterface.kt
+++ app/src/main/java/ch/sdp/vibester/api/LyricsOVHApiInterface.kt
@@ -2,10 +2,22 @@ package ch.sdp.vibester.api
 
 import ch.sdp.vibester.model.Lyric
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface LyricsOVHApiInterface {
     @GET("v1/{artist}/{title}")
     fun getLyrics(@Path("artist") artist: String, @Path("title") title: String): Call<Lyric>
+
+    companion object {
+        fun create(baseUrl:String): LyricsOVHApiInterface {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            return retrofit.create(LyricsOVHApiInterface::class.java)
+        }
+    }
 }

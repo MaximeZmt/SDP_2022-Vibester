cd5b69fc05daabd95ecbdbdb093babc0769eb3d2
Kamila Babayeva <kamifai14@gmail.com>
2022-03-16 16:44:58 +0100

    LastfmHelper.kt: combine two lists before random pick

--- app/src/main/java/ch/sdp/vibester/api/LastfmHelper.kt
+++ app/src/main/java/ch/sdp/vibester/api/LastfmHelper.kt
@@ -7,7 +7,6 @@ class LastfmHelper {
     companion object{
         private var pageLimitQueryTwo = 200;
 
-        fun getBothtSongList(method: String, tag:String = ""): List<String> {
             val firstQuery = SongList(LastfmApi.querySongList(OkHttpClient(),
                 LastfmUri(method = method, tag = tag)).get())
             val firstSongList = firstQuery.getSongList()
@@ -22,11 +21,8 @@ class LastfmHelper {
                 LastfmUri(method = method, tag = tag, page = pageQueryTwo.toString())).get())
             val secondSongList = secondQuery.getSongList()
 
-            val elementsOne = 2
-            val elementsTwo = 8
-            val randomElementsOne = firstSongList.asSequence().shuffled().take(elementsOne).toList()
-            var randomElementsTwo = secondSongList.asSequence().shuffled().take(elementsTwo).toList()
-            val finalList = merge(randomElementsOne, randomElementsTwo)
+            val mergedLists = merge(firstSongList, secondSongList)
+            var finalList = mergedLists.asSequence().shuffled().take(GAME_SIZE).toList()
             return finalList
         }
 

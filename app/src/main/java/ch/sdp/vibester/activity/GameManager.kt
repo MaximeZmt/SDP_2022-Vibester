91b1f59bd36ece1e123a2b1234acf78f2a1fcba7
Kamila Babayeva <kamifai14@gmail.com>
2022-03-23 17:46:58 +0100

    TypingGameActivity.kt: function to operate songs

--- app/src/main/java/ch/sdp/vibester/activity/GameManager.kt
+++ app/src/main/java/ch/sdp/vibester/activity/GameManager.kt
@@ -8,44 +8,80 @@ import okhttp3.OkHttpClient
 import java.io.Serializable
 import java.util.concurrent.CompletableFuture
 
-class GameManager constructor(method: String, tag: String): Serializable{
+class GameManager: Serializable{
     private var score = 0;
-    private var currentSong = 0;
-    private val gameSize = 10;
-    private var gameSongList: List<String> = mutableListOf<String>()
+    private var nextSongInd = 0
+    private val GAME_SIZE = 10
+    private var playedSongs = 0
+    private lateinit var currentSong: Song
+    private var gameSongList: List<String> = mutableListOf()
+    private lateinit var mediaPlayer: CompletableFuture<MediaPlayer>
+    private val correctSongs = mutableListOf<Song>()
+    private var wrongSongs = mutableListOf<Song>()
 
-    init {
-        gameSongList = LastfmHelper.getRandomSongList(method, tag);
+    fun setGameSongList(jsonMeta:String, method:String){
+        gameSongList = SongList(jsonMeta, method).getShuffledSongList();
     }
 
     fun increaseScore(){
         score++;
     }
 
+    fun getPlayedSongs():Int{
+        return playedSongs
+    }
+
+    fun getCorrectSongs(): MutableList<Song>{
+        return correctSongs
+    }
+
+    fun getWrongSongs(): MutableList<Song>{
+        return wrongSongs
+    }
+
     fun getScore():Int {
         return score
+    }
+
+    fun getCurrentSong(): Song{
+        return currentSong
     }
 
     fun getSongList():List<String>{
         return gameSongList
     }
 
-    fun nextSong():Song {
-//        if(currentSong < gameSongList.size){
-            val songName = gameSongList.get(currentSong)
-            val songDetails = Song.singleSong(ItunesMusicApi.querySong(songName, OkHttpClient(), 1).get())
-            currentSong++;
-//            return songDetails
-
-//        }
-
-//        else{
-//            finish()
-//        }
-        return songDetails
+    fun addCorrectSong(){
+        correctSongs.add(currentSong)
     }
 
-    fun finish(){
+    fun addWrongSong(){
+        wrongSongs.add(currentSong)
+    }
+
+    fun checkNextSong():Boolean{
+        if(playedSongs < GAME_SIZE){
+            return true
+        }
+        return false
+    }
+
+    fun setNextSong(): Song {
+        val songName = gameSongList[nextSongInd]
+        try{
+            currentSong = Song.singleSong(ItunesMusicApi.querySong(songName, OkHttpClient(), 1).get())
+            nextSongInd++
+            playedSongs++
+        }
+        catch (e:Exception) {
+            nextSongInd++;
+            setNextSong()
+        }
+        return currentSong
+    }
+
+    fun playSong(){
+        mediaPlayer = AudioPlayer.playAudio(currentSong.getPreviewUrl())
     }
 
     fun getMediaPlayer(): CompletableFuture<MediaPlayer> {

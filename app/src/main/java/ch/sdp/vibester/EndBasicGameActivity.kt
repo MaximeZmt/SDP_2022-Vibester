030eb827cf570dedb4795a01bbaf78e212b1e041
Kamila Babayeva <kamifai14@gmail.com>
2022-03-23 17:43:34 +0100

    EndBasicGameActivity.kt: add text to display in the end

--- app/src/main/java/ch/sdp/vibester/EndBasicGameActivity.kt
+++ app/src/main/java/ch/sdp/vibester/EndBasicGameActivity.kt
@@ -1,10 +1,12 @@
 package ch.sdp.vibester
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
 class EndBasicGameActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_end_basic_game)
@@ -12,7 +14,7 @@ class EndBasicGameActivity : AppCompatActivity() {
         var bundle = intent.extras
         var txtView = findViewById<TextView>(R.id.score)
         if (bundle != null) {
-            txtView.text = bundle.getString("score")
+            txtView.text = "Your score is "+ bundle.getString("score")
         }
     }
 }

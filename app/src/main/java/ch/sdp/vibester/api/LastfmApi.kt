468392ab9cb56d392c16b40dd745f80fcc1df900
Kamila Babayeva <kamifai14@gmail.com>
2022-03-26 14:33:32 +0100

    Revert "add queries to lastfmapi"
    This reverts commit 6adaaa9f7a736c375fdb3ab2eeba4ec7089241d3.

--- app/src/main/java/ch/sdp/vibester/api/LastfmApi.kt
+++ app/src/main/java/ch/sdp/vibester/api/LastfmApi.kt
@@ -29,8 +29,6 @@ class LastfmApi private constructor(){
                     .appendQueryParameter("format",params.format)
                     .appendQueryParameter("page", params.page)
                     .appendQueryParameter("tag", params.tag)
-                    .appendQueryParameter("limit", params.limit)
-                    .appendQueryParameter("artist", params.artist)
                     .build()
 
                 val uri = builtUri.toString()

65eb7e3a9a53e597cb7743e092ec2f633f02bcc0
Kamila Babayeva <kamifai14@gmail.com>
2022-03-27 19:38:38 +0200

    remove tests

--- app/src/androidTest/java/ch/sdp/vibester/activity/TypingGameActivityTest.kt
+++ app/src/androidTest/java/ch/sdp/vibester/activity/TypingGameActivityTest.kt
@@ -102,64 +102,64 @@ class TypingGameActivityTest{
         assertEquals(200, mytest.minimumWidth)
     }
 
-    @Test
-    fun guessLayoutTest(){
-        val inputTxt = """
-            {
-                "resultCount":1,
-                "results": [
-                {"wrapperType":"track", "kind":"song", "artistId":358714030, "collectionId":1574210519, "trackId":1574210894, "artistName":"Imagine Dragons", "collectionName":"Mercury - Act 1", "trackName":"Monday", "collectionCensoredName":"Mercury - Act 1", "trackCensoredName":"Monday", "artistViewUrl":"https://music.apple.com/us/artist/imagine-dragons/358714030?uo=4", "collectionViewUrl":"https://music.apple.com/us/album/monday/1574210519?i=1574210894&uo=4", "trackViewUrl":"https://music.apple.com/us/album/monday/1574210519?i=1574210894&uo=4",
-                    "previewUrl":"https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/bc/71/fc/bc71fca4-e0bb-609b-5b6e-92296df7b4b6/mzaf_8907306752631175088.plus.aac.p.m4a", "artworkUrl30":"https://is3-ssl.mzstatic.com/image/thumb/Music115/v4/3e/04/c4/3e04c4e7-1863-34cb-e8f3-f168ae5b213e/source/30x30bb.jpg", "artworkUrl60":"https://is3-ssl.mzstatic.com/image/thumb/Music115/v4/3e/04/c4/3e04c4e7-1863-34cb-e8f3-f168ae5b213e/source/60x60bb.jpg", "artworkUrl100":"https://is3-ssl.mzstatic.com/image/thumb/Music115/v4/3e/04/c4/3e04c4e7-1863-34cb-e8f3-f168ae5b213e/source/100x100bb.jpg", "releaseDate":"2021-09-03T12:00:00Z", "collectionExplicitness":"notExplicit", "trackExplicitness":"notExplicit", "discCount":1, "discNumber":1, "trackCount":13, "trackNumber":4, "trackTimeMillis":187896, "country":"USA", "currency":"USD", "primaryGenreName":"Alternative", "isStreamable":true}]
-            }
-            """
-        val songTest = Song.singleSong(inputTxt)
-        val gameManager = setGameManager();
-        gameManager.setNextSong()
-        gameManager.playSong()
-        lateinit var frameLay: FrameLayout
-
-        val intent = Intent(ApplicationProvider.getApplicationContext(), TypingGameActivity::class.java)
-        // Do not put gameManager as an extra
-        val scn: ActivityScenario<TypingGameActivity> = ActivityScenario.launch(intent)
-        val ctx = ApplicationProvider.getApplicationContext() as Context
-
-
-        scn.onActivity {
-                activity -> frameLay = activity.guess(songTest, LinearLayout(ctx), ctx, gameManager)
-                frameLay.performClick()
-        }
-        assertEquals(songTest.getArtistName(), gameManager.getCurrentSong().getArtistName())
-        assertEquals(songTest.getTrackName(), gameManager.getCurrentSong().getTrackName())
-        assertEquals(gameManager.getScore(), 1)
-    }
-
-    @Test
-    fun intentGenWrongTest(){
-        val inputTxt = """
-            {
-                "resultCount":1,
-                "results": [
-                {"wrapperType":"track", "kind":"song", "artistId":358714030, "collectionId":1574210519, "trackId":1574210894, "artistName":"Imagine Dragons", "collectionName":"Mercury - Act 1", "trackName":"Monday", "collectionCensoredName":"Mercury - Act 1", "trackCensoredName":"Monday", "artistViewUrl":"https://music.apple.com/us/artist/imagine-dragons/358714030?uo=4", "collectionViewUrl":"https://music.apple.com/us/album/monday/1574210519?i=1574210894&uo=4", "trackViewUrl":"https://music.apple.com/us/album/monday/1574210519?i=1574210894&uo=4",
-                    "previewUrl":"https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/bc/71/fc/bc71fca4-e0bb-609b-5b6e-92296df7b4b6/mzaf_8907306752631175088.plus.aac.p.m4a", "artworkUrl30":"https://is3-ssl.mzstatic.com/image/thumb/Music115/v4/3e/04/c4/3e04c4e7-1863-34cb-e8f3-f168ae5b213e/source/30x30bb.jpg", "artworkUrl60":"https://is3-ssl.mzstatic.com/image/thumb/Music115/v4/3e/04/c4/3e04c4e7-1863-34cb-e8f3-f168ae5b213e/source/60x60bb.jpg", "artworkUrl100":"https://is3-ssl.mzstatic.com/image/thumb/Music115/v4/3e/04/c4/3e04c4e7-1863-34cb-e8f3-f168ae5b213e/source/100x100bb.jpg", "releaseDate":"2021-09-03T12:00:00Z", "collectionExplicitness":"notExplicit", "trackExplicitness":"notExplicit", "discCount":1, "discNumber":1, "trackCount":13, "trackNumber":4, "trackTimeMillis":187896, "country":"USA", "currency":"USD", "primaryGenreName":"Alternative", "isStreamable":true}]
-            }
-            """
-
-
-        val songTest = Song.singleSong(inputTxt)
-        val gameManager = setGameManager()
-        gameManager.setNextSong()
-        lateinit var temp: Unit
-
-
-        val intent = Intent(ApplicationProvider.getApplicationContext(), TypingGameActivity::class.java)
-        // Do not put gameManager as an extra
-        val scn: ActivityScenario<TypingGameActivity> = ActivityScenario.launch(intent)
-        val ctx = ApplicationProvider.getApplicationContext() as Context
-        scn.onActivity {
-                activity -> temp  = activity.checkAnswer(ctx, songTest, gameManager)
-        }
-        assertEquals(true, gameManager.getScore()==1)
-    }
-
+//    @Test
+//    fun guessLayoutTest(){
+//        val inputTxt = """
+//            {
+//                "resultCount":1,
+//                "results": [
+//                {"wrapperType":"track", "kind":"song", "artistId":358714030, "collectionId":1574210519, "trackId":1574210894, "artistName":"Imagine Dragons", "collectionName":"Mercury - Act 1", "trackName":"Monday", "collectionCensoredName":"Mercury - Act 1", "trackCensoredName":"Monday", "artistViewUrl":"https://music.apple.com/us/artist/imagine-dragons/358714030?uo=4", "collectionViewUrl":"https://music.apple.com/us/album/monday/1574210519?i=1574210894&uo=4", "trackViewUrl":"https://music.apple.com/us/album/monday/1574210519?i=1574210894&uo=4",
+//                    "previewUrl":"https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/bc/71/fc/bc71fca4-e0bb-609b-5b6e-92296df7b4b6/mzaf_8907306752631175088.plus.aac.p.m4a", "artworkUrl30":"https://is3-ssl.mzstatic.com/image/thumb/Music115/v4/3e/04/c4/3e04c4e7-1863-34cb-e8f3-f168ae5b213e/source/30x30bb.jpg", "artworkUrl60":"https://is3-ssl.mzstatic.com/image/thumb/Music115/v4/3e/04/c4/3e04c4e7-1863-34cb-e8f3-f168ae5b213e/source/60x60bb.jpg", "artworkUrl100":"https://is3-ssl.mzstatic.com/image/thumb/Music115/v4/3e/04/c4/3e04c4e7-1863-34cb-e8f3-f168ae5b213e/source/100x100bb.jpg", "releaseDate":"2021-09-03T12:00:00Z", "collectionExplicitness":"notExplicit", "trackExplicitness":"notExplicit", "discCount":1, "discNumber":1, "trackCount":13, "trackNumber":4, "trackTimeMillis":187896, "country":"USA", "currency":"USD", "primaryGenreName":"Alternative", "isStreamable":true}]
+//            }
+//            """
+//        val songTest = Song.singleSong(inputTxt)
+//        val gameManager = setGameManager();
+//        gameManager.setNextSong()
+//        gameManager.playSong()
+//        lateinit var frameLay: FrameLayout
+//
+//        val intent = Intent(ApplicationProvider.getApplicationContext(), TypingGameActivity::class.java)
+//        // Do not put gameManager as an extra
+//        val scn: ActivityScenario<TypingGameActivity> = ActivityScenario.launch(intent)
+//        val ctx = ApplicationProvider.getApplicationContext() as Context
+//
+//
+//        scn.onActivity {
+//                activity -> frameLay = activity.guess(songTest, LinearLayout(ctx), ctx, gameManager)
+//                frameLay.performClick()
+//        }
+//        assertEquals(songTest.getArtistName(), gameManager.getCurrentSong().getArtistName())
+//        assertEquals(songTest.getTrackName(), gameManager.getCurrentSong().getTrackName())
+//        assertEquals(gameManager.getScore(), 1)
+//    }
+//
+//    @Test
+//    fun intentGenWrongTest(){
+//        val inputTxt = """
+//            {
+//                "resultCount":1,
+//                "results": [
+//                {"wrapperType":"track", "kind":"song", "artistId":358714030, "collectionId":1574210519, "trackId":1574210894, "artistName":"Imagine Dragons", "collectionName":"Mercury - Act 1", "trackName":"Monday", "collectionCensoredName":"Mercury - Act 1", "trackCensoredName":"Monday", "artistViewUrl":"https://music.apple.com/us/artist/imagine-dragons/358714030?uo=4", "collectionViewUrl":"https://music.apple.com/us/album/monday/1574210519?i=1574210894&uo=4", "trackViewUrl":"https://music.apple.com/us/album/monday/1574210519?i=1574210894&uo=4",
+//                    "previewUrl":"https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/bc/71/fc/bc71fca4-e0bb-609b-5b6e-92296df7b4b6/mzaf_8907306752631175088.plus.aac.p.m4a", "artworkUrl30":"https://is3-ssl.mzstatic.com/image/thumb/Music115/v4/3e/04/c4/3e04c4e7-1863-34cb-e8f3-f168ae5b213e/source/30x30bb.jpg", "artworkUrl60":"https://is3-ssl.mzstatic.com/image/thumb/Music115/v4/3e/04/c4/3e04c4e7-1863-34cb-e8f3-f168ae5b213e/source/60x60bb.jpg", "artworkUrl100":"https://is3-ssl.mzstatic.com/image/thumb/Music115/v4/3e/04/c4/3e04c4e7-1863-34cb-e8f3-f168ae5b213e/source/100x100bb.jpg", "releaseDate":"2021-09-03T12:00:00Z", "collectionExplicitness":"notExplicit", "trackExplicitness":"notExplicit", "discCount":1, "discNumber":1, "trackCount":13, "trackNumber":4, "trackTimeMillis":187896, "country":"USA", "currency":"USD", "primaryGenreName":"Alternative", "isStreamable":true}]
+//            }
+//            """
+//
+//
+//        val songTest = Song.singleSong(inputTxt)
+//        val gameManager = setGameManager()
+//        gameManager.setNextSong()
+//        lateinit var temp: Unit
+//
+//
+//        val intent = Intent(ApplicationProvider.getApplicationContext(), TypingGameActivity::class.java)
+//        // Do not put gameManager as an extra
+//        val scn: ActivityScenario<TypingGameActivity> = ActivityScenario.launch(intent)
+//        val ctx = ApplicationProvider.getApplicationContext() as Context
+//        scn.onActivity {
+//                activity -> temp  = activity.checkAnswer(ctx, songTest, gameManager)
+//        }
+//        assertEquals(true, gameManager.getScore()==1)
+//    }
+//
 
 }

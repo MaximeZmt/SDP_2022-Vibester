5bca5ebc3ff358d9cb9af69f2e44f1893487b9af
Your laurynas.lopata@epfl.ch <laurynas.lopata@epfl.ch>
2022-04-01 00:47:41 +0200

    Rerun CI

--- app/src/androidTest/java/ch/sdp/vibester/activity/ProfileActivityTest.kt
+++ app/src/androidTest/java/ch/sdp/vibester/activity/ProfileActivityTest.kt
@@ -20,7 +20,7 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class ProfileActivityTest {
 
-    private val sleepTime: Long = 5000
+    private val sleepTime: Long = 4000
 
     @Before
     fun setUp() {

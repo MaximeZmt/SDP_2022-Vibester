a50f7d93fd425896f97bf0fc76d5605dc549ce68
Kamila Babayeva <kamifai14@gmail.com>
2022-04-03 17:33:53 +0200

    GameSetupActivity: remove unnecessary library

--- app/src/main/java/ch/sdp/vibester/activity/GameSetupActivity.kt
+++ app/src/main/java/ch/sdp/vibester/activity/GameSetupActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
-import android.view.ViewGroup
 import android.view.Window
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity

ce29c0b8c4868877976503b02665e5e0f7eb4cbc
Kamila Babayeva <kamifai14@gmail.com>
2022-04-15 17:22:38 +0200

    SearchUsers: add activity to search for users

---
+++ app/src/main/java/ch/sdp/vibester/SearchUsers.kt
@@ -0,0 +1,11 @@
+package ch.sdp.vibester
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchUsers : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_users)
+    }
+}

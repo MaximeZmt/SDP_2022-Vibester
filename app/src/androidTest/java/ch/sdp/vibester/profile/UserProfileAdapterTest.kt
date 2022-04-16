f451da79cda7cae033438a21c30a6044ae2f72d9
Kamila Babayeva <kamifai14@gmail.com>
2022-04-16 21:54:27 +0200

    UserProfileAdapterTest: remove redundant tests

--- app/src/androidTest/java/ch/sdp/vibester/profile/UserProfileAdapterTest.kt
+++ app/src/androidTest/java/ch/sdp/vibester/profile/UserProfileAdapterTest.kt
@@ -32,37 +32,4 @@ class UserProfileAdapterTest {
             CoreMatchers.equalTo(defaultType)
         )
     }
-
-    @Test
-    fun setupAdapterForRecyclerView() {
-        val user1 = UserProfile("test1", "Brownie", "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", "test1@gmail.com")
-        val user2 = UserProfile("test2", "Cookie", "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", "test2@gmail.com")
-        val users: MutableList<UserProfile> = arrayListOf()
-        users.addAll(listOf(user1, user2))
-        val recyclerView = RecyclerView(ApplicationProvider.getApplicationContext())
-        recyclerView.layoutManager =
-            LinearLayoutManager(ApplicationProvider.getApplicationContext())
-        val userAdapter = UserProfileAdapter(users)
-        recyclerView.adapter = userAdapter
-        val newUsers: MutableList<UserProfile> = arrayListOf()
-        newUsers.add(UserProfile("test3", "Cake", "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", "test3@gmail.com"))
-        userAdapter.addUsers(newUsers)
-        userAdapter.notifyDataSetChanged()
-    }
-
-    @Test
-    fun addPlayersWorks() {
-        val user1 = UserProfile("test1", "Brownie", "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", "test1@gmail.com")
-        val user2 = UserProfile("test2", "Cookie", "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", "test2@gmail.com")
-        val users: MutableList<UserProfile> = arrayListOf()
-        users.addAll(listOf(user1, user2))
-        val recyclerView = RecyclerView(ApplicationProvider.getApplicationContext())
-        recyclerView.layoutManager =
-            LinearLayoutManager(ApplicationProvider.getApplicationContext())
-        val userAdapter = UserProfileAdapter(users)
-        val user3 = UserProfile("test3", "Cake", "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", "test3@gmail.com")
-        val updatedList = arrayListOf(user3)
-        userAdapter.addUsers(updatedList)
-        MatcherAssert.assertThat(userAdapter.users == updatedList, CoreMatchers.equalTo(true))
-    }
 }

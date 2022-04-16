241cf0d92248c8baef250bdf4b4a3a60e378979d
Kamila Babayeva <kamifai14@gmail.com>
2022-04-16 21:55:51 +0200

    UserProfileAdapter: fix comments

--- app/src/main/java/ch/sdp/vibester/profile/UserProfileAdapter.kt
+++ app/src/main/java/ch/sdp/vibester/profile/UserProfileAdapter.kt
@@ -8,11 +8,14 @@ import androidx.recyclerview.widget.RecyclerView
 import ch.sdp.vibester.R
 
 /**
- * UserAdapter that is used in the RecycleView to search users
+ * UserAdapter to set userProfile views with username and image in RecycleView. It is used to search for users.
  */
 class UserProfileAdapter(val users: MutableList<UserProfile>):
     RecyclerView.Adapter<UserProfileAdapter.UserProfileViewHolder>() {
 
+    /**
+     * Create a RecycleView layout with the userProfile view as an item
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserProfileViewHolder {
         val view: View = LayoutInflater.from(parent.context)
             .inflate(R.layout.user_search_item_layout, parent, false)
@@ -23,12 +26,15 @@ class UserProfileAdapter(val users: MutableList<UserProfile>):
         holder.bind(users[position])
     }
 
+    /**
+     * Get amount of users displayed
+     */
     override fun getItemCount(): Int {
-        return  users.size
+        return users.size
     }
 
     /**
-     * Customer ViewHolder class for UserProfile
+     * Customer ViewHolder class for UserProfile. Each item contains username and image.
      */
     inner class UserProfileViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         /**

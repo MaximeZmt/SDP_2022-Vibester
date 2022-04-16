25a500e08d345f5de3001725348ccc6590c7b769
Kamila Babayeva <kamifai14@gmail.com>
2022-04-16 20:12:18 +0200

    SearchUserActivityTest: add tests

---
+++ app/src/androidTest/java/ch/sdp/vibester/activity/SearchUserActivityTest.kt
@@ -0,0 +1,67 @@
+package ch.sdp.vibester.activity
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import ch.sdp.vibester.R
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SearchUserActivityTest {
+
+    @Rule
+    @JvmField
+    val activityRule = ActivityScenarioRule(SearchUserActivity::class.java)
+
+    @Before
+    fun setUp() {
+        Intents.init()
+    }
+
+    @After
+    fun clean() {
+        Intents.release()
+    }
+
+
+    @Test
+    fun recycleViewToViewTest() {
+        Espresso.onView(ViewMatchers.withId(R.id.searchList))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun recycleViewClickTest() {
+        Espresso.onView((ViewMatchers.withId(R.id.searchList)))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                    2,
+                    ViewActions.click()
+                )
+            )
+    }
+
+    @Test
+    fun recycleViewScrollDownTest() {
+        val recyclerView = RecyclerView(ApplicationProvider.getApplicationContext())
+        val itemCount = recyclerView.adapter?.itemCount
+        if (itemCount != null) {
+            Espresso.onView(ViewMatchers.withId(R.id.searchList)).perform(
+                RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                    itemCount.minus(1)
+                )
+            )
+        }
+    }
+}

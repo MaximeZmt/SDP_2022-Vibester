9d878ac997ed5044f4ca7b72f477d65e0711b8b2
jiabaow <jiabaowenwendy@gmail.com>
2022-04-23 11:47:32 +0200

    update data type

--- app/src/androidTest/java/ch/sdp/vibester/scoreboard/UserScoreboardAdapterTest.kt
+++ app/src/androidTest/java/ch/sdp/vibester/scoreboard/UserScoreboardAdapterTest.kt
@@ -3,6 +3,7 @@ package ch.sdp.vibester.scoreboard
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ApplicationProvider
+import ch.sdp.vibester.user.User
 import ch.sdp.vibester.user.UserScoreboardAdapter
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
@@ -12,10 +13,10 @@ import org.junit.Test
 class UserScoreboardAdapterTest {
     @Test
     fun recyclerViewShowsCorrectCount() {
-        val player1 = Player(111, "Brownie", "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", 687)
-        val player2 = Player(555, "Cheesecake", "https://images.app.goo.gl/REJnoWR2t3mi2kYJA", 678)
-        val players: MutableList<Player> = arrayListOf()
-        players.addAll(listOf(player1, player2))
+        val user1 = User("test1", "Brownie", "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", "test1@gmail.com")
+        val user2 = User("test2", "Cookie", "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", "test2@gmail.com")
+        val players: MutableList<User> = arrayListOf()
+        players.addAll(listOf(user1, user2))
         val userScoreboardViewHolder: RecyclerView.Adapter<UserScoreboardAdapter.PlayerViewHolder> =
             UserScoreboardAdapter(players)
         assertThat(userScoreboardViewHolder.itemCount, equalTo(2))
@@ -23,10 +24,10 @@ class UserScoreboardAdapterTest {
 
     @Test
     fun itemTypeIsCorrect() {
-        val player1 = Player(111, "Brownie", "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", 687)
-        val player2 = Player(555, "Cheesecake", "https://images.app.goo.gl/REJnoWR2t3mi2kYJA", 678)
-        val players: MutableList<Player> = arrayListOf()
-        players.addAll(listOf(player1, player2))
+        val user1 = User("test1", "Brownie", "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", "test1@gmail.com")
+        val user2 = User("test2", "Cookie", "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", "test2@gmail.com")
+        val players: MutableList<User> = arrayListOf()
+        players.addAll(listOf(user1, user2))
         val userScoreboardViewHolder: RecyclerView.Adapter<UserScoreboardAdapter.PlayerViewHolder> =
             UserScoreboardAdapter(players)
         val defaultType = 0
@@ -35,32 +36,32 @@ class UserScoreboardAdapterTest {
 
     @Test
     fun setupAdapterForRecyclerView() {
-        val player1 = Player(111, "Brownie", "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", 687)
-        val player2 = Player(555, "Cheesecake", "https://images.app.goo.gl/REJnoWR2t3mi2kYJA", 678)
-        val players: MutableList<Player> = arrayListOf()
-        players.addAll(listOf(player1, player2))
+        val user1 = User("test1", "Brownie", "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", "test1@gmail.com")
+        val user2 = User("test2", "Cookie", "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", "test2@gmail.com")
+        val players: MutableList<User> = arrayListOf()
+        players.addAll(listOf(user1, user2))
         val recyclerView = RecyclerView(ApplicationProvider.getApplicationContext())
         recyclerView.layoutManager =
             LinearLayoutManager(ApplicationProvider.getApplicationContext())
         val playerAdapter = UserScoreboardAdapter(players)
         recyclerView.adapter = playerAdapter
-        val newPlayers: MutableList<Player> = arrayListOf()
-        newPlayers.add(Player(444, "Scone", "https://images.app.goo.gl/YkBi16zwyjB7ejj96", 659))
+        val newPlayers: MutableList<User> = arrayListOf()
+        newPlayers.add(User("test3", "Scone", "https://images.app.goo.gl/YkBi16zwyjB7ejj96", "test3@gmail.com"))
         playerAdapter.addPlayers(newPlayers)
         playerAdapter.notifyDataSetChanged()
     }
 
     @Test
     fun addPlayersWorks() {
-        val player1 = Player(111, "Brownie", "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", 687)
-        val player2 = Player(555, "Cheesecake", "https://images.app.goo.gl/REJnoWR2t3mi2kYJA", 678)
-        val players: MutableList<Player> = arrayListOf()
-        players.addAll(listOf(player1, player2))
+        val user1 = User("test1", "Brownie", "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", "test1@gmail.com")
+        val user2 = User("test2", "Cookie", "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", "test2@gmail.com")
+        val players: MutableList<User> = arrayListOf()
+        players.addAll(listOf(user1, user2))
         val recyclerView = RecyclerView(ApplicationProvider.getApplicationContext())
         recyclerView.layoutManager =
             LinearLayoutManager(ApplicationProvider.getApplicationContext())
         val playerAdapter = UserScoreboardAdapter(players)
-        val player3 = Player(444, "Scone", "https://images.app.goo.gl/YkBi16zwyjB7ejj96", 659)
+        val player3 = User("test3", "Scone", "https://images.app.goo.gl/YkBi16zwyjB7ejj96", "test3@gmail.com")
         val updatedList = arrayListOf(player3)
         playerAdapter.addPlayers(updatedList)
         assertThat(playerAdapter.players == updatedList, equalTo(true))

5ad51b5d913455fd75024a2a5c8fdc6235323951
Kamila Babayeva <kamifai14@gmail.com>
2022-04-24 09:19:13 +0200

    CreateProfielActivity: change var to val

--- app/src/main/java/ch/sdp/vibester/activity/CreateProfileActivity.kt
+++ app/src/main/java/ch/sdp/vibester/activity/CreateProfileActivity.kt
@@ -29,9 +29,9 @@ class CreateProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_profile)
 
-        var email = intent.getStringExtra("email").toString()
-        var username = findViewById<EditText>(R.id.accountUsername)
-        var handle = findViewById<EditText>(R.id.accountHandle)
+        val email = intent.getStringExtra("email").toString()
+        val username = findViewById<EditText>(R.id.accountUsername)
+        val handle = findViewById<EditText>(R.id.accountHandle)
 
         val btCreateAcc = findViewById<Button>(R.id.createButton)
         val btnUploadImg = findViewById<Button>(R.id.uploadImg)

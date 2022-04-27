3f7e82fc2747ee16ae2f40e5bea828c474fd7031
Your laurynas.lopata@epfl.ch <laurynas.lopata@epfl.ch>
2022-04-27 22:01:48 +0200

    Started tests

---
+++ app/src/androidTest/java/ch/sdp/vibester/PartyRoomTest.kt
@@ -0,0 +1,30 @@
+package ch.sdp.vibester
+
+import ch.sdp.vibester.helper.PartyRoom
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertTrue
+import org.junit.Test
+
+class PartyRoomTest {
+    @Test
+    fun correctSetRoom() {
+        val roomName = "testRoom"
+        val testPartyRoom = PartyRoom()
+
+        testPartyRoom.setRoomName(roomName)
+
+        assertEquals(testPartyRoom.getRoomName(), roomName)
+    }
+
+    @Test
+    fun correctSetEmailList() {
+        val emailList = mutableListOf("email1, email2")
+        val testPartyRoom = PartyRoom()
+
+        testPartyRoom.setEmailList(emailList)
+
+        for(email in emailList) {
+            assert(testPartyRoom.getEmailList().contains(email))
+        }
+    }
+}

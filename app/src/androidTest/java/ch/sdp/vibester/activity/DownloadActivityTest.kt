d06d7ab1039ff92e00a8977e14a34f2711b8215c
Derin Arda Alpay <d.arda.alpay@gmail.com>
2022-04-28 20:48:28 +0200

    Further fixing merge

--- app/src/androidTest/java/ch/sdp/vibester/activity/DownloadActivityTest.kt
+++ app/src/androidTest/java/ch/sdp/vibester/activity/DownloadActivityTest.kt
@@ -14,6 +14,7 @@ import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import ch.sdp.vibester.R
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
--- app/src/main/java/ch/sdp/vibester/activity/DownloadActivity.kt
+++ app/src/main/java/ch/sdp/vibester/activity/DownloadActivity.kt
@@ -11,6 +11,7 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast

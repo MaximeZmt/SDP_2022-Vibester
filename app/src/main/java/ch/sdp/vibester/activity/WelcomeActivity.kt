c4a6200a56a8f8ec0928a28b33f75ab961c97b84
maxime.zammit <maxime.zammit@epfl.ch>
2022-04-29 22:07:04 +0200

    fix user not updating on using back button from android

--- app/src/main/java/ch/sdp/vibester/activity/WelcomeActivity.kt
+++ app/src/main/java/ch/sdp/vibester/activity/WelcomeActivity.kt
@@ -34,17 +34,26 @@ class WelcomeActivity : AppCompatActivity() {
         supportActionBar?.hide()
         setContentView(R.layout.activity_welcome_screen)
 
+        updateUserConnectionStatus()
+
+        !PersistanceSetter.getPersistance()
+
+        Log.e("INFO: Should Access", ((InternetState.hasAccessedInternetOnce(this))).toString())
+        PersistanceSetter.setPersistance()
+        Database.get()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        updateUserConnectionStatus()
+    }
+
+    private fun updateUserConnectionStatus() {
         val userStatusTextValue = findViewById<TextView>(R.id.user_status)
         if(FireBaseAuthenticator.isLoggedIn() ||  testLoggedIn)
         {
             userStatusTextValue.text = "User: " + FireBaseAuthenticator.getCurrentUserMail()
         }
-
-        val startOfApp = !PersistanceSetter.getPersistance()
-
-        Log.e("INFO: Should Access", ((InternetState.hasAccessedInternetOnce(this))).toString())
-        PersistanceSetter.setPersistance()
-        Database.get()
     }
 
     private fun sendDirectIntent(arg: Class<*>?) {

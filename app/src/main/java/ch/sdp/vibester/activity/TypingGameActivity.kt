5666f0ffc780925f5003edecec076a7325290705
Kamila Babayeva <kamifai14@gmail.com>
2022-05-03 20:34:58 +0200

    TypingGame: fix the tests

--- app/src/main/java/ch/sdp/vibester/activity/TypingGameActivity.kt
+++ app/src/main/java/ch/sdp/vibester/activity/TypingGameActivity.kt
@@ -234,10 +234,12 @@ class TypingGameActivity : GameActivity() {
      * Function to set scores in the end of the game
      */
     private fun setScores() {
-        dataGetter.updateFieldInt(FireBaseAuthenticator.getCurrentUID(), "totalGames", 1, method = "sum")
-        dataGetter.updateFieldInt(FireBaseAuthenticator.getCurrentUID(), "correctSongs", gameManager.getCorrectSongs().size, method = "sum")
-        dataGetter.updateFieldInt(FireBaseAuthenticator.getCurrentUID(), "bestScore", gameManager.getScore(), method = "best")
-        dataGetter.updateSubFieldInt(FireBaseAuthenticator.getCurrentUID(), gameManager.getScore(), "scores", gameManager.gameMode, method = "best")
+        if(::gameManager.isInitialized){
+            dataGetter.updateFieldInt(FireBaseAuthenticator.getCurrentUID(), "totalGames", 1, method = "sum")
+            dataGetter.updateFieldInt(FireBaseAuthenticator.getCurrentUID(), "correctSongs", gameManager.getCorrectSongs().size, method = "sum")
+            dataGetter.updateFieldInt(FireBaseAuthenticator.getCurrentUID(), "bestScore", gameManager.getScore(), method = "best")
+            dataGetter.updateSubFieldInt(FireBaseAuthenticator.getCurrentUID(), gameManager.getScore(), "scores", gameManager.gameMode, method = "best")
+        }
     }
     /**
      * Functions for testing

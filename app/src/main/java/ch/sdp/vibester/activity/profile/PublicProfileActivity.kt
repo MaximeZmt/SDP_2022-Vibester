192ac094ee76d782d538a798fb4579328df8f630
jiabaow <jiabaowenwendy@gmail.com>
2022-05-15 15:04:22 +0200

    add test

--- app/src/main/java/ch/sdp/vibester/activity/profile/PublicProfileActivity.kt
+++ app/src/main/java/ch/sdp/vibester/activity/profile/PublicProfileActivity.kt
@@ -1,12 +1,17 @@
 package ch.sdp.vibester.activity.profile
 
+import android.os.Bundle
+
 /** profile page of a user with only public information */
 class PublicProfileActivity : ProfileActivity() {
+    lateinit var userId: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        userId = intent.getStringExtra("UserId").toString()
+        super.onCreate(savedInstanceState)
+    }
 
     override fun queryDatabase() {
-        val userId = intent.getStringExtra("UserId")
-        if (userId != null) {
-            dataGetter.getUserData(userId, this::setupProfile)
-        }
+        dataGetter.getUserData(userId, this::setupProfile)
     }
 }

f66ad7f3bfed2873a104a37172f3dce7e0b39c7c
jiabaow <jiabaowenwendy@gmail.com>
2022-05-15 15:57:18 +0200

    update tests

--- app/src/androidTest/java/ch/sdp/vibester/user/UserProfileAdapterTest.kt
+++ app/src/androidTest/java/ch/sdp/vibester/user/UserProfileAdapterTest.kt
@@ -15,7 +15,7 @@ class UserProfileAdapterTest {
         val user2 = User("test2",  "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", "test2@gmail.com")
         val users: MutableList<User> = arrayListOf()
         users.addAll(listOf(user1, user2))
-        val userProfileViewHolder: RecyclerView.Adapter<UserProfileAdapter.UserProfileViewHolder> = UserProfileAdapter(users, FireBaseAuthenticator(), DataGetter())
+        val userProfileViewHolder: RecyclerView.Adapter<UserProfileAdapter.UserProfileViewHolder> = UserProfileAdapter(users, FireBaseAuthenticator(), DataGetter(), null)
         MatcherAssert.assertThat(userProfileViewHolder.itemCount, CoreMatchers.equalTo(2))
     }
 
@@ -25,7 +25,7 @@ class UserProfileAdapterTest {
         val user2 = User("test2", "https://images.app.goo.gl/yiPpy7JDRFaZRiAg9", "test2@gmail.com")
         val users: MutableList<User> = arrayListOf()
         users.addAll(listOf(user1, user2))
-        val userProfileViewHolder: RecyclerView.Adapter<UserProfileAdapter.UserProfileViewHolder> = UserProfileAdapter(users, FireBaseAuthenticator(), DataGetter())
+        val userProfileViewHolder: RecyclerView.Adapter<UserProfileAdapter.UserProfileViewHolder> = UserProfileAdapter(users, FireBaseAuthenticator(), DataGetter(), null)
         val defaultType = 0
         MatcherAssert.assertThat(
             userProfileViewHolder.getItemViewType(0),

54b1bf17c4cdbf848b85720d52f897369a68a95f
Kamila Babayeva <kamifai14@gmail.com>
2022-05-17 22:17:45 +0200

    PublicProfileActivity: set popup

--- app/src/main/java/ch/sdp/vibester/activity/PublicProfileActivity.kt
+++ app/src/main/java/ch/sdp/vibester/activity/PublicProfileActivity.kt
@@ -1,23 +1,69 @@
 package ch.sdp.vibester.activity
 
-import android.os.Bundle
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.LayoutInflater
+import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.widget.Button
+import android.widget.ScrollView
 import android.widget.TableLayout
+import android.widget.TextView
 import ch.sdp.vibester.R
+import ch.sdp.vibester.auth.FireBaseAuthenticator
+import ch.sdp.vibester.database.DataGetter
+import ch.sdp.vibester.database.ImageGetter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import javax.inject.Inject
 
 /** profile page of a user with only public information */
-class PublicProfileActivity : ProfileActivity() {
-    lateinit var userId: String
+class PublicProfileActivity @Inject constructor(private val ctx: Context,
+                                                private val userId:String,
+                                                private val scoreOrFriends: String,
+                                                val imageGetter: ImageGetter,
+                                                val dataGetter: DataGetter,
+                                                val authenticator: FireBaseAuthenticator){
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        userId = intent.getStringExtra("UserId").toString()
-        super.onCreate(savedInstanceState)
-        if (intent.getStringExtra("ScoresOrFriends") == "Scores") {
-            findViewById<TableLayout>(R.id.profileStatistics).visibility = VISIBLE
+    lateinit var profileActivity:ProfileActivity
+    private lateinit var alertDialog: Dialog
+
+    /**
+     * Remove/add elements in layout, set button listeners
+     */
+    private fun setDialogLayout(){
+        alertDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        if (scoreOrFriends == "Scores") {
+            alertDialog.findViewById<TableLayout>(R.id.profile_statistics)?.visibility = VISIBLE
         }
+        alertDialog.findViewById<Button>(R.id.profile_logout)?.visibility = GONE
+        alertDialog.findViewById<FloatingActionButton>(R.id.profile_returnToMain)?.visibility = GONE
+
+        val close = alertDialog.findViewById<TextView>(R.id.profile_close)
+        close.visibility = VISIBLE
+        close.setOnClickListener {
+            alertDialog.dismiss()
+        }
+
+        alertDialog.findViewById<ScrollView>(R.id.profileContent).layoutParams.width = ctx.resources.getDimensionPixelSize(R.dimen.profile_width);
+        alertDialog.findViewById<ScrollView>(R.id.profileContent).layoutParams.height = ctx.resources.getDimensionPixelSize(R.dimen.profile_height);
     }
 
-    override fun queryDatabase() {
-        dataGetter.getUserData(userId, this::setupProfile)
+    /**
+     * Set dialog and fetch data from db
+     */
+    fun setProfileDialog(){
+        val dialogView = LayoutInflater.from(ctx).inflate(R.layout.activity_profile, null)
+        alertDialog = Dialog(ctx)
+        alertDialog.setContentView(dialogView)
+        setDialogLayout()
+
+        profileActivity = ProfileActivity(alertDialog.context, dialogView, imageGetter, dataGetter, authenticator)
+        profileActivity.queryDatabase(userId)
+        profileActivity.setScoreBtnListener()
+
+        alertDialog.show()
     }
+
 }

ec1174faa51fceeb0054b7a50c9a76213383797c
Your laurynas.lopata@epfl.ch <laurynas.lopata@epfl.ch>
2022-05-18 20:20:40 +0200

    Attempting to make Typing game wokr

--- app/src/main/java/ch/sdp/vibester/activity/PartyRoomActivity.kt
+++ app/src/main/java/ch/sdp/vibester/activity/PartyRoomActivity.kt
@@ -1,25 +1,39 @@
 package ch.sdp.vibester.activity
 
 import android.content.ContentValues
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import ch.sdp.vibester.R
+import ch.sdp.vibester.api.LastfmApiInterface
+import ch.sdp.vibester.api.LastfmMethod
+import ch.sdp.vibester.api.LastfmUri
+import ch.sdp.vibester.database.AppPreferences
 import ch.sdp.vibester.database.DataGetter
 import ch.sdp.vibester.database.Database
+import ch.sdp.vibester.helper.GameManager
 import ch.sdp.vibester.helper.PartyRoom
 import ch.sdp.vibester.user.User
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import com.google.gson.Gson
 import dagger.hilt.android.AndroidEntryPoint
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class PartyRoomActivity : AppCompatActivity() {
     @Inject
     lateinit var dataGetter: DataGetter
+
+    lateinit var gameManager: GameManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +47,23 @@ class PartyRoomActivity : AppCompatActivity() {
         else {
             fetchData(roomName)
         }
+
+        val startGame = findViewById<Button>(R.id.startGame)
+
+        startGame.setOnClickListener {
+            setGameManager()
+
+            Log.w("DEBUG", gameManager.gameSize.toString())
+            Log.w("DEBUG", gameManager.getSongList().toString())
+
+            Thread.sleep(3000)
+
+            val newIntent = Intent(this, TypingGameActivity::class.java)
+            newIntent.putExtra("gameManager", gameManager)
+            newIntent.putExtra("Difficulty", R.string.easy.toString())
+
+            startActivity(newIntent)
+        }
     }
 
     private fun updateUI(partyRoom: PartyRoom) {
@@ -45,5 +76,44 @@ class PartyRoomActivity : AppCompatActivity() {
 
     private fun createRoom(roomName: String) {
         dataGetter.createRoom(roomName, this::updateUI)
+    }
+
+    private fun setGameManager() {
+        gameManager = GameManager()
+        gameManager.setGameSize(1)
+
+        chooseGenre(method = LastfmMethod.BY_ARTIST.method, artist = "Imagine Dragons", mode = R.string.imagine_dragons)
+//        setGameSongList
+
+    }
+
+    private fun setGameSongList(uri: LastfmUri) {
+        val service = LastfmApiInterface.createLastfmService()
+        val call = service.getSongList(uri.convertToHashmap())
+        call.enqueue(object : Callback<Any> {
+            override fun onFailure(call: Call<Any>, t: Throwable?) {}
+            override fun onResponse(call: Call<Any>, response: Response<Any>) {
+                /* TODO: OFFLINE
+                gameManager.setInternet()
+                gameManager.setContext(context)
+                 */
+
+                gameManager.setGameSongList(Gson().toJson(response.body()), uri.method)
+                Log.w("DEBUG", gameManager.getSongList().toString())
+            }
+        })
+    }
+
+    private fun chooseGenre(method: String = "", artist: String = "", tag: String = "", mode: Int = 0) {
+        val uri = LastfmUri()
+
+        uri.method = method
+        uri.artist = artist
+        uri.tag = tag
+
+        gameManager.gameMode = getString(mode)
+        AppPreferences.setStr(getString(R.string.preferences_game_genre), getString(mode))
+
+        setGameSongList(uri)
     }
 }

40eb69757743b3eb62a2918b60dc893bd9ce8fb5
Kamila Babayeva <kamifai14@gmail.com>
2022-05-22 17:13:01 +0200

    PublicProfile:convert to fragment

--- app/src/main/java/ch/sdp/vibester/activity/profile/PublicProfileActivity.kt
+++ app/src/main/java/ch/sdp/vibester/activity/profile/PublicProfileActivity.kt
@@ -7,15 +7,15 @@ import ch.sdp.vibester.R
 class PublicProfileActivity : ProfileActivity() {
     lateinit var userId: String
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        userId = intent.getStringExtra("UserId").toString()
-        super.onCreate(savedInstanceState)
-        if (intent.getStringExtra("ScoresOrFollowing") == R.string.profile_scores.toString()) {
-            showAHideB(R.id.profile_scroll_stat, R.id.profile_scroll_following)
-        } else if (intent.getStringExtra("ScoresOrFollowing") == R.string.profile_following.toString()) {
-            showAHideB(R.id.profile_scroll_following, R.id.profile_scroll_stat)
-        }
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        userId = intent.getStringExtra("UserId").toString()
+//        super.onCreate(savedInstanceState)
+//        if (intent.getStringExtra("ScoresOrFollowing") == R.string.profile_scores.toString()) {
+//            showAHideB(R.id.profile_scroll_stat, R.id.profile_scroll_following)
+//        } else if (intent.getStringExtra("ScoresOrFollowing") == R.string.profile_following.toString()) {
+//            showAHideB(R.id.profile_scroll_following, R.id.profile_scroll_stat)
+//        }
+//    }
 
 
     override fun queryDatabase() {

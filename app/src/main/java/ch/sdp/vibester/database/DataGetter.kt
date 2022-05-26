b224e0b8d571f5754bf52a855231e73289af89cc
laurislopata <40314248+laurislopata@users.noreply.github.com>
2022-05-26 17:19:54 +0200

    Update app/src/main/java/ch/sdp/vibester/database/DataGetter.kt
    Co-authored-by: zwierski <44404546+zwierski@users.noreply.github.com>

--- app/src/main/java/ch/sdp/vibester/database/DataGetter.kt
+++ app/src/main/java/ch/sdp/vibester/database/DataGetter.kt
@@ -275,6 +275,12 @@ class DataGetter @Inject constructor() {
      * @param fieldName name of the field to update
      * @param value new value to write to the database
      */
+    /**
+     * This functions that updates the field of a room entry
+     * @param roomID ID of the room
+     * @param fieldName name of the field to update
+     * @param value new value to write to the database
+     */
     fun <T> updateRoomField(roomID: String, fieldName: String, value: T) {
         dbRoomRef.child("${roomID}/${fieldName}").setValue(value)
     }

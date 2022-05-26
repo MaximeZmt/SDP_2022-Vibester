d67906e1b707b4714936ba55823c348515237fc0
laurislopata <40314248+laurislopata@users.noreply.github.com>
2022-05-26 17:07:07 +0200

    Update app/src/androidTest/java/ch/sdp/vibester/activity/ChoosePartyRoomActivityTest.kt
    Co-authored-by: Maxime <61186830+MaximeZmt@users.noreply.github.com>

--- app/src/androidTest/java/ch/sdp/vibester/activity/ChoosePartyRoomActivityTest.kt
+++ app/src/androidTest/java/ch/sdp/vibester/activity/ChoosePartyRoomActivityTest.kt
@@ -46,7 +46,7 @@ class ChoosePartyRoomActivityTest {
     val mockUsersRepo = mockk<DataGetter>()
 
     private fun createMockInvocation(partyRoom: PartyRoom) {
-        every {mockUsersRepo.createRoom(any())} answers {
+        every { mockUsersRepo.createRoom(any()) } answers {
             firstArg<(PartyRoom, String) -> Unit>().invoke(partyRoom, "")
         }
 

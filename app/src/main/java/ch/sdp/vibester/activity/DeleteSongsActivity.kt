5db76e159e3cbef57a608e22db7e20c2a338ffe5
jiabaow <jiabaowenwendy@gmail.com>
2022-05-28 09:16:40 +0200

    refactor DeleteSongsActivity.kt

--- app/src/main/java/ch/sdp/vibester/activity/DeleteSongsActivity.kt
+++ app/src/main/java/ch/sdp/vibester/activity/DeleteSongsActivity.kt
@@ -1,22 +1,23 @@
 package ch.sdp.vibester.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup.LayoutParams
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.view.Window
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.LinearLayout.LayoutParams.MATCH_PARENT
-import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import ch.sdp.vibester.R
 import ch.sdp.vibester.helper.Helper
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.io.*
+
 /**
  * Class that handles deleting files, more specifically songs in the scope of this project.
  */
@@ -28,7 +29,10 @@ class DeleteSongsActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_delete_songs)
 
-        Helper().setReturnToMainListener(findViewById<FloatingActionButton>(R.id.delete_returnToMain), this)
+        Helper().setReturnToMainListener(
+            findViewById<FloatingActionButton>(R.id.delete_returnToMain),
+            this
+        )
 
         val layout: LinearLayout = findViewById(R.id.delete_songs_linear)
         generateButtons(layout)
@@ -39,18 +43,21 @@ class DeleteSongsActivity : AppCompatActivity() {
      * Assigns each button a call to the "deleteDownloadedSong" function as a listener.
      */
     private fun generateButtons(layout: LinearLayout) {
-        val records = File(applicationContext.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), "records.txt")
+        val records = File(
+            applicationContext.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS),
+            "records.txt"
+        )
 
-        if(!records.exists() || records.length() == 0L) {
+        if (!records.exists() || records.length() == 0L) {
             createNoSongsView(layout)
         } else {
             val reader = BufferedReader(FileReader(records))
             var currentLine = reader.readLine()
             var iterator = 0
 
-            while(currentLine != null) {
+            while (currentLine != null) {
                 val trimmed = currentLine.trim()
-                if(trimmed.isNotEmpty()) {
+                if (trimmed.isNotEmpty()) {
                     val deleteButton = createButton(trimmed, iterator, layout)
                     layout.addView(deleteButton)
                 }
@@ -81,8 +88,14 @@ class DeleteSongsActivity : AppCompatActivity() {
      * from the text file which keeps track of all downloads.
      */
     private fun deleteDownloadedSong(btn: View, layout: LinearLayout): Boolean {
-        val records = File(applicationContext.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), "records.txt")
-        val tempRecords = File(applicationContext.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), "temp.txt")
+        val records = File(
+            applicationContext.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS),
+            "records.txt"
+        )
+        val tempRecords = File(
+            applicationContext.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS),
+            "temp.txt"
+        )
 
         val recordReader = BufferedReader(FileReader(records))
         val recordWriter = BufferedWriter(FileWriter(tempRecords, true))
@@ -114,7 +127,10 @@ class DeleteSongsActivity : AppCompatActivity() {
         records.delete()
         if (tempRecords.renameTo(records) && removeFromProperties(buttonText)) {
             layout.removeView(btn)
-            val songToDelete = File(applicationContext.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), "extract_of_${button.text}")
+            val songToDelete = File(
+                applicationContext.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS),
+                "extract_of_${button.text}"
+            )
             if (songToDelete.delete()) {
                 Toast.makeText(applicationContext, "Song successfully removed!", Toast.LENGTH_LONG).show()
                 return true
@@ -125,12 +141,18 @@ class DeleteSongsActivity : AppCompatActivity() {
     }
 
     /**
-     * Removes the line correspoding to the song-to-be-deleted from the properties.txt file.
+     * Removes the line corresponding to the song-to-be-deleted from the properties.txt file.
      * @param buttonText: Text which contains song name - artist name
      */
     private fun removeFromProperties(buttonText: String): Boolean {
-        val properties = File(applicationContext.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), "properties.txt")
-        val tempProp = File(applicationContext.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), "tempProp.txt")
+        val properties = File(
+            applicationContext.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS),
+            "properties.txt"
+        )
+        val tempProp = File(
+            applicationContext.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS),
+            "tempProp.txt"
+        )
 
         val propReader = BufferedReader(FileReader(properties))
         val propWriter = BufferedWriter(FileWriter(tempProp, true))
@@ -149,7 +171,8 @@ class DeleteSongsActivity : AppCompatActivity() {
             val split = trimmed.split(" - ")
 
             if (split[0].trim().lowercase() == buttonSplit[0].trim().lowercase()
-             && split[1].trim().lowercase() == buttonSplit[1].trim().lowercase()) {
+                && split[1].trim().lowercase() == buttonSplit[1].trim().lowercase()
+            ) {
                 currentLine = propReader.readLine()
                 continue
             }

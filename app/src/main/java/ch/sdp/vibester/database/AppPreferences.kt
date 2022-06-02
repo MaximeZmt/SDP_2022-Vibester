e419163ce3e937bad2c5e4b8038d47916e36c753
Kamila Babayeva <kamifai14@gmail.com>
2022-06-02 11:00:12 +0200

    AppPreferences: modify to set gameManager

--- app/src/main/java/ch/sdp/vibester/database/AppPreferences.kt
+++ app/src/main/java/ch/sdp/vibester/database/AppPreferences.kt
@@ -1,7 +1,9 @@
 package ch.sdp.vibester.database
 
+import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
+import com.google.gson.GsonBuilder
 
 /**
  * Share some values across the whole application
@@ -9,36 +11,46 @@ import android.content.SharedPreferences
 object AppPreferences {
     private const val NAME = "Vibester"
     private const val MODE = Context.MODE_PRIVATE
-    private lateinit var preferences: SharedPreferences
+    lateinit var preferences: SharedPreferences
 
     fun init(context: Context) {
         preferences = context.getSharedPreferences(NAME, MODE)
     }
 
-    /**
-     * SharedPreferences extension function, so we won't need to call edit() and apply()
-     * ourselves on every SharedPreferences operation.
-     */
-    private inline fun SharedPreferences.edit(operation:(SharedPreferences.Editor) -> Unit) {
-        val editor = edit()
-        operation(editor)
-        editor.apply()
-    }
 
     fun getStr(key: String): String? {
-        if (this::preferences.isInitialized) {
-            return preferences.getString(key, "")
+        return if (this::preferences.isInitialized) {
+            preferences.getString(key, "")
         } else {
-            return ""
+            ""
         }
 
     }
 
     fun setStr(key: String, value: String){
         if (this::preferences.isInitialized) {
-            preferences.edit {
-                it.putString(key, value)
-            }
+            preferences.edit().putString(key, value).apply()
         }
+    }
+
+    /**
+     * Saves object into the Preferences.
+     * @param `object` Object of model class (of type [T]) to save
+     * @param key Key with which Shared preferences to
+     **/
+    fun <T> setObject(`object`: T, key: String) {
+        val jsonString = GsonBuilder().create().toJson(`object`)
+        preferences.edit().putString(key, jsonString).apply()
+    }
+
+    /**
+     * Used to retrieve object from the Preferences.
+     * @param key Shared Preference key with which object was saved.
+     **/
+    inline fun <reified T> getObject(key: String): T? {
+        val value = preferences.getString(key, null)
+        //We convert this JSON String to model object. Parameter "c" (of
+        //type Class < T >" is used to cast.
+        return GsonBuilder().create().fromJson(value, T::class.java)
     }
 }

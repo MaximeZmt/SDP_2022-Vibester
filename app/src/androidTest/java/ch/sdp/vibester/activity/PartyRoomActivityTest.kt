a4cea659fc2259c00184b031cde765e9cdf497e0
jiabaow <jiabaowenwendy@gmail.com>
2022-06-03 00:26:31 +0200

    fix tests in PartyRoomActivityTest.kt

--- app/src/androidTest/java/ch/sdp/vibester/activity/PartyRoomActivityTest.kt
+++ app/src/androidTest/java/ch/sdp/vibester/activity/PartyRoomActivityTest.kt
@@ -6,13 +6,13 @@ import android.content.Intent
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import ch.sdp.vibester.R
+import ch.sdp.vibester.api.LastfmMethod
 import ch.sdp.vibester.database.AppPreferences
 import ch.sdp.vibester.database.DataGetter
 import ch.sdp.vibester.helper.GameManager
@@ -62,12 +62,30 @@ class PartyRoomActivityTest {
         every { mockUsersRepo.updateRoomField<Boolean>(any(), any(), any()) } answers {}
         every { mockUsersRepo.updateRoomField<MutableList<Pair<String, String>>>(any(), any(), any()) } answers {}
 
-
     }
 
     @After
     fun clean() {
         Intents.release()
+    }
+
+    private fun setGameManager(numSongs:Int = 1, valid: Boolean = true): GameManager {
+        val epilogue = "{\"tracks\":{\"track\":["
+        val prologue =
+            "], \"@attr\":{\"tag\":\"british\",\"page\":\"1\",\"perPage\":\"1\",\"totalPages\":\"66649\",\"total\":\"66649\"}}}"
+        var middle = "{\"name\":\"Monday\",\"artist\":{\"name\":\"Imagine Dragons\"}}"
+        if(!valid) middle = "{\"name\":\"TEST_SONG_TEST\",\"artist\":{\"name\":\"TEST_ARTIST_TEST\"}}"
+        val gameManager = GameManager()
+
+        var i = 0
+        var completeMiddle = middle
+        while(i < numSongs-1){
+            completeMiddle += ",$middle"
+            i++
+        }
+        gameManager.setGameSongList(epilogue + completeMiddle + prologue, LastfmMethod.BY_TAG.method)
+
+        return gameManager
     }
 
     @Test
@@ -87,10 +105,11 @@ class PartyRoomActivityTest {
         AppPreferences.setStr(ctx.getString(R.string.preferences_game_mode), "local_typing")
         AppPreferences.setStr(ctx.getString(R.string.preferences_game_genre), "imagine dragons")
 
-
+        val gameManager = setGameManager()
         val intent = Intent(ctx, PartyRoomActivity::class.java)
         intent.putExtra("roomName", mockRoomName)
         intent.putExtra("createRoom", true)
+        intent.putExtra("gameManager", gameManager)
 
         val scn: ActivityScenario<PartyRoomActivity> = ActivityScenario.launch(intent)
 
@@ -118,6 +137,7 @@ class PartyRoomActivityTest {
         val intent = Intent(ctx, PartyRoomActivity::class.java)
         intent.putExtra("roomName", mockRoomName)
         intent.putExtra("createRoom", false)
+        intent.putExtra("gameManager", setGameManager())
 
         val scn: ActivityScenario<PartyRoomActivity> = ActivityScenario.launch(intent)
 
@@ -138,7 +158,6 @@ class PartyRoomActivityTest {
         AppPreferences.setStr(ctx.getString(R.string.preferences_game_mode), "local_typing")
         AppPreferences.setStr(ctx.getString(R.string.preferences_game_genre), "imagine dragons")
 
-
         mockPartyRoom.setEmailList(mockUserEmailList)
 
         createMockInvocation(mockPartyRoom, mockSongList, true, mockRoomName)
@@ -146,11 +165,12 @@ class PartyRoomActivityTest {
         val intent = Intent(ctx, PartyRoomActivity::class.java)
         intent.putExtra("roomName", mockRoomName)
         intent.putExtra("createRoom", true)
+        intent.putExtra("gameManager", setGameManager())
 
         val scn: ActivityScenario<PartyRoomActivity> = ActivityScenario.launch(intent)
 
         Intents.intended(IntentMatchers.hasComponent(TypingGameActivity ::class.java.name))
-        Intents.intended(IntentMatchers.hasExtra("Difficulty", R.string.GameSetup_easy))
+        Intents.intended(IntentMatchers.hasExtra("Difficulty", 0)) // 0 for easy
     }
 
     @Test
@@ -174,13 +194,12 @@ class PartyRoomActivityTest {
         val intent = Intent(ctx, PartyRoomActivity::class.java)
         intent.putExtra("roomName", mockRoomID)
         intent.putExtra("createRoom", false)
+        intent.putExtra("gameManager", setGameManager())
 
         val scn: ActivityScenario<PartyRoomActivity> = ActivityScenario.launch(intent)
 
         Espresso.onView(ViewMatchers.withId(R.id.roomId))
             .check(ViewAssertions.matches(ViewMatchers.withText(mockRoomID)))
-
-
     }
 
 }

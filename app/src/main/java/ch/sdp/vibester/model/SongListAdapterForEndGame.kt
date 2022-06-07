e9e96348a23e96c42349f419d0fc4128284d7c35
jiabaow <jiabaowenwendy@gmail.com>
2022-06-07 20:54:22 +0200

    refactor onCreateViewHolder

--- app/src/main/java/ch/sdp/vibester/model/SongListAdapterForEndGame.kt
+++ app/src/main/java/ch/sdp/vibester/model/SongListAdapterForEndGame.kt
@@ -29,7 +29,8 @@ class SongListAdapterForEndGame constructor(
      * Create a RecycleView layout with the Song view as an item
      */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongListForEndGameViewHolder {
-        val view = AdapterHelper().createViewForViewHolder(parent, R.layout.song_item_download_layout)
+        val layout = R.layout.song_item_download_layout
+        val view = AdapterHelper().createViewForViewHolder(parent, layout)
         return SongListForEndGameViewHolder(view)
     }
 
